6b069dc95e36fbad7ff821230436c6561a5c464c
Võ Duy Tạo <duytaovo@gmail.com>
2023-12-03 16:36:39 +0700

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
 import HeaderNav from "./HeaderNav";
-import SearchInput from "./SearchInput";
 import CartButton from "./CartButton";
 import FilterButton from "./FilterButton";
 import styles from "./header.module.scss";
